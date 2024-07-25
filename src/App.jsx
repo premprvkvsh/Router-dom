69614262f9c20fa8a483314d59654826b664f896
Prem <pprakash312003@gmail.com>
2024-07-26 03:47:08 +0530

    props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,79 @@
-import { lazy , Suspense } from "react";
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
-import './App.css'
-const Dashboard = lazy(() => import('./components/Dashboard'))
-const Landing = lazy(() => import('./components/Landing'))
+// import { lazy , Suspense } from "react";
+// import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+// import './App.css'
+// const Dashboard = lazy(() => import('./components/Dashboard'))
+// const Landing = lazy(() => import('./components/Landing'))
 
 
-function App() {
-  // const [count, setCount] = useState(0)
-return (
+// function App() {
+//   // const [count, setCount] = useState(0)
+// return (
+//     <div>
+//     <BrowserRouter>
+//     <Appbar />
+//     <Suspense fallback={<div>Loading...</div>}>
+//           <Routes>
+//             <Route path="/dashboard" element={<Dashboard />} />
+//             <Route path="/" element={<Landing />} />
+//           </Routes>
+//         </Suspense>
+//     </BrowserRouter>   
+//     </div>
+//   )
+
+// }
+
+// function Appbar(){
+//   const navigate = useNavigate();
+
+//   return <div>
+//     <div>
+//         <button onClick={() => {
+//           navigate("/")
+//         }}>Landing page</button>
+
+//         <button onClick={() => {
+//           navigate("/dashboard");
+//         }}>Dashboard</button>  
+//       </div>   
+//   </div>
+// }
+
+// export default App
+ 
+import { useState } from "react";
+
+function App(){
+
+  const [count, setCount] = useState(0);
+
+  return (
     <div>
-    <BrowserRouter>
-    <Appbar />
-    <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/" element={<Landing />} />
-          </Routes>
-        </Suspense>
-    </BrowserRouter>   
+       <Count count={count} />
+
+       <Buttons count={count} setCount={setCount}/>
     </div>
   )
-
 }
 
-function Appbar(){
-  const navigate = useNavigate();
-
+function Count({count}){
   return <div>
-    <div>
-        <button onClick={() => {
-          navigate("/")
-        }}>Landing page</button>
-
-        <button onClick={() => {
-          navigate("/dashboard");
-        }}>Dashboard</button>  
-      </div>   
+    {count}
   </div>
+}
+
+function Buttons({count , setCount}){
+ return <div>
+  <button onClick={() => {
+    setCount(count + 1)
+  }}>Increase</button>
+
+
+  <button onClick={() => {
+    setCount(count - 1)
+  }}>decrease</button>
+ </div>
+
 }
 
 export default App
